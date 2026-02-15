61f8bc653cdd908ed8dcb4cc5239f210dfcb83bc
ЗояЗанина <zoia.zanina@gmail.com>
2026-02-15 19:35:58 +0500

    Лабораторная 2

---
+++ Лабораторная 2/Класс Library.py
@@ -0,0 +1,58 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+# TODO написать класс Book
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id_ = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(id_={self.id_}, name='{self.name}', pages={self.pages})"
+
+
+# TODO написать класс Library
+class Library:
+    def __init__(self, books: list = None):
+        if books is None:
+            self.books = []
+        else:
+            self.books = books
+
+    def get_next_book_id(self) -> int:
+        return 1 if not self.books else self.books[-1].id_ + 1
+
+    def get_index_by_book_id(self, id_: int) -> int:
+        for index, book in enumerate(self.books):
+            if book.id_ == id_:
+                return index
+
+        raise ValueError("Книги с запрашиваемым id не существует")
+
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
